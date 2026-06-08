98193b5a52663f1c42d7b07c06548f71950bce4d
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-06-08 17:11:20 +0600

    fixed error

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get("/all-surahs/:lang", async(req, res) => {
     const { lang } = req.params;
     try {
         const response = await axios.get(`https://alquran-api.pages.dev/api/quran?lang=${lang}`);
-
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ message: lang === "bn" ? "সূরার তালিকা লোড করতে ব্যর্থ" : "Failed to load list of surah", error: error.message });
@@ -27,9 +26,10 @@ app.get("/all-surahs/:lang", async(req, res) => {
 app.get("/surah/:surahId/:lang", async(req, res) => {
     const { surahId, lang } = req.params;
     try {
-        const response = await axios.get(`https://alquran-api.pages.dev/api/quran/surah/${surahId}?lang=${lang}`);
-
-        res.json(response.data);
+        if(surahId !== null){
+            const response = await axios.get(`https://alquran-api.pages.dev/api/quran/surah/${surahId}?lang=${lang}`);
+            res.json(response.data);
+        }
     } catch (error) {
         res.status(500).json({ message:  lang === "bn" ? "সূরা লোড করতে ব্যর্থ" : "Failed to load surah", error: error.message });
     }
@@ -38,9 +38,10 @@ app.get("/surah/:surahId/:lang", async(req, res) => {
 app.get("/surah/:surahId/verse/:verseId/:lang", async(req, res) => {
     const { surahId, verseId, lang } = req.params;
     try {
-        const response = await axios.get(`https://alquran-api.pages.dev/api/quran/surah/${surahId}/verse/${verseId}?lang=${lang}`);
-
-        res.json(response.data);
+        if(surahId !== null){
+            const response = await axios.get(`https://alquran-api.pages.dev/api/quran/surah/${surahId}/verse/${verseId}?lang=${lang}`);
+            res.json(response.data);
+        }
     } catch (error) {
         res.status(500).json({ message: lang === "bn" ? "আয়াত লোড করতে ব্যর্থ" : "Failed to load verse", error: error.message });
     }
@@ -49,9 +50,10 @@ app.get("/surah/:surahId/verse/:verseId/:lang", async(req, res) => {
 app.get("/search/:search/:lang", async(req, res) => {
     const { search, lang } = req.params;
     try {
-        const response = await axios.get(`https://alquran-api.pages.dev/api/quran/search?q=${search}&lang=${lang}`);
-
-        res.json(response.data);
+        if(search !== ""){
+            const response = await axios.get(`https://alquran-api.pages.dev/api/quran/search?q=${search}&lang=${lang}`);
+            res.json(response.data);
+        }
     } catch (error) {
         res.status(500).json({ message: lang === "bn" ? `${search} এর জন্য কোন ফলাফল পাওয়া যায়নি` : `No result found for ${search}`, error: error.message });
     }
